14e9d35174b34aa0e22e6d4d864a9d0d05d21cb6
Ryan <ryanmwb@gmail.com>
2018-12-11 20:33:33 -0500

    added seed data for both users and wikis

---
+++ src/db/seeds/20181212004755-wiki.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const faker = require("faker");
+
+let wikis = [];
+
+for(let i=0; i < 16; i++){
+  wikis.push({
+    title: faker.hacker.noun(),
+    body: faker.hacker.phrase(),
+    private: faker.random.boolean(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    userId: 18
+  })
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Wikis", wikis, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(' Wikis', null, {})
+  }
+};
